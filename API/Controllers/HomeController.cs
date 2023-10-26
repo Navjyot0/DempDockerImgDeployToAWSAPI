35fdba426e328003f75a4ddeef37cd530ec7969c
navjyot FSE <navjyot@fse.com>
2023-10-26 09:10:51 +0530

    added home controller

---
+++ API/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "Hello World";
+        }
+    }
+}
